40145528f5b64899778f8e516caabf2342fbdbb7
Hojjat-Ayoubi <150190440+Hojjat-Ayoubi@users.noreply.github.com>
2023-11-30 02:24:58 +0330

    Harmonic and Factorial add

---
+++ index.php
@@ -0,0 +1,56 @@
+<?php
+                            /////////////Hojjat Ayoubi/////////////
+
+
+//////////////////start Harmonic////////////////// 
+
+// function harmonic($a) {
+//     $result = 0;
+//     for ($i = 1; $i <= $a; $i++) {
+//         $result += 1 / $i;
+//         // echo($i." = ". $result."<br/>");
+//     }
+//     return $result;
+// }
+// $a = 3;
+// $harmonicValue = harmonic($a);
+// echo "$a = $harmonicValue";
+
+//////////////////end Harmonic////////////////// 
+
+
+
+//////////////////start Factorial//////////////////
+
+// function factorial($x){
+//     $result = 1 ;
+//     for ($i = 1; $i <= $x; $i++) {
+//         $result *= $i ;
+//         // echo($i." = ". $result."<br/>");
+//     }
+//     return $result;
+// }
+// $x = 4 ;
+// $result = factorial($x);
+// echo " $x = $result";
+
+//////////////////end Factorial////////////////// 
+
+
+//////////////////start T/////////////////
+
+// function score($s) {
+//     $bazar = max($s);
+//     return $bazar;
+// }
+
+// $s = [85, 92, 78, 1 ,79, 90, 88 , 93];
+
+// rsort($s);
+// echo "مرتب‌سازی: " . implode(" , ", $s) . "<br/>";
+
+// $bazar1 = score($s);
+// echo "بزرگترین نمره: $bazar1";
+
+//////////////////end T////////////////// 
+?>
